916df769565fcba86f65f7954314c30d8bdf70aa
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2025-07-29 14:57:07 +0700

    remove CreateUserDto and related user creation logic from UserService

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@nestjs/common';
 import { User } from './entities/user.entity';
-import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UserService {
@@ -22,26 +21,8 @@ export class UserService {
       updatedAt: new Date('2024-02-01'),
     },
   ];
-  private idCounter = 1;
-
-  create(createUserDto: CreateUserDto): User {
-    const user: User = {
-      id: this.idCounter++,
-      username: createUserDto.username,
-      email: createUserDto.email,
-      password: createUserDto.password,
-      createdAt: new Date(),
-      updatedAt: new Date(),
-    };
-    this.users.push(user);
-    return user;
-  }
 
   findAll(): User[] {
     return this.users;
-  }
-
-  findOne(id: number): User | undefined {
-    return this.users.find((user) => user.id === id);
   }
 }
