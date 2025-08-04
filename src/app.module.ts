079f5303da7e545d0cf067fcf2760718d26131a4
21120535 <98611090+quynluvpinky@users.noreply.github.com>
2025-08-04 11:18:33 +0700

    refactor: remove account module and related files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AccountModule } from './account/account.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -19,8 +17,6 @@ import { AuthModule } from './auth/auth.module';
         uri: configService.get<string>('MONGO_DB_URI'),
       }),
     }),
-    AccountModule,
-    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
